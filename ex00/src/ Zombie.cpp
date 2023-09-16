6f5576993d35e7fc9eb67efa1b1229de51e1dd26
mrezaei <moeinrezaei330@gmail.com>
2023-09-16 17:08:24 +0200

    Zombie is added.

--- ex00/src/ Zombie.cpp
+++ ex00/src/ Zombie.cpp
@@ -3,11 +3,13 @@
 Zombie::Zombie(std::string name) : _name(name)
 {
     std::cout << "Zombie " << this->_name << " is born" << std::endl;
+    return ;
 }
 
 Zombie::~Zombie(void)
 {
     std::cout << "Zombie " << this->_name << " is dead" << std::endl;
+    return ;
 }
 
 void    Zombie::announce(void)
