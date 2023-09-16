78ba78426dc80767ef647e144b9aa0e81ab408e1
mrezaei <moeinrezaei330@gmail.com>
2023-09-16 17:07:30 +0200

    update the files.

--- ex00/src/NewZombie.cpp
+++ ex00/src/NewZombie.cpp
@@ -1,1 +1,9 @@
 #include "../include/Zombie.hpp"
+
+Zombie *newZombie(std::string name)
+{
+	Zombie *newZombie;
+
+	newZombie = new Zombie(name);
+	return (newZombie);
+}
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,1 +1,14 @@
 #include "../include/Zombie.hpp"
+
+int main ()
+{
+	Zombie	Zombie_stack("Foo_on_stack");
+	Zombie_stack.announce();
+	
+	Zombie	*Zombie_heap;
+	Zombie_heap = newZombie("Foo_on_heap");
+	Zombie_heap->announce();
+	delete Zombie_heap;
+	
+	return (0);
+}
