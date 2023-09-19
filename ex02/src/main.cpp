9db1fbb8046e1a46fe745db53d2e2dd3fab6475c
mrezaei <moeinrezaei330@gmail.com>
2023-09-19 14:54:51 +0200

    ex02 is added.

---
+++ ex02/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *strPTR = &str;
+    std::string &strREF = str;
+
+    std::cout << "Address of str:     " << &str << std::endl;
+    std::cout << "Address of strPTR:  " << strPTR << std::endl;
+    std::cout << "Address of strREF:  " << &strREF << std::endl;
+    std::cout << "============================================" << std::endl;
+    std::cout << "Value of str:                " << str << std::endl;
+    std::cout << "Value pointed to by strPTR:  " << *strPTR << std::endl;
+    std::cout << "Value of strREF:             " << strREF << std::endl;
+}
